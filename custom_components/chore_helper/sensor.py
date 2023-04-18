1bf162f4514ea1399e37c430c7611ed11f40662b
Ben McClure <ben.mcclure@gmail.com>
2023-04-18 00:41:40 +0000

    Tweak scheduling for a specific weekday of the nth week when calculating from the end of the month

--- custom_components/chore_helper/sensor.py
+++ custom_components/chore_helper/sensor.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from calendar import monthrange
 from datetime import date, datetime, time, timedelta
-from typing import Any
+from typing import Any, Tuple
 from collections.abc import Generator
 
 from dateutil.relativedelta import relativedelta
@@ -723,14 +723,17 @@ class MonthlyChore(Chore):
         self._period = config.get(const.CONF_PERIOD, 1)
 
     @staticmethod
-    def viable_weeks_in_month(date_of_month: date, chore_day: int):
+    def viable_weeks_in_month(date_of_month: date, chore_day: int, last_week_must_contain_chore_day: bool = False) -> int:
         """Find the highest week number that contains the chore day in the month."""
         first_of_month = date(date_of_month.year, date_of_month.month, 1)
         last_of_month = first_of_month + relativedelta(day=31)
         first_week = first_of_month.isocalendar()[1]
-        last_chore_day_offset = (last_of_month.weekday() - chore_day) % 7
-        last_chore_day = last_of_month - timedelta(days=last_chore_day_offset)
-        last_chore_week = last_chore_day.isocalendar()[1]
+        if last_week_must_contain_chore_day:
+            last_chore_day_offset = (last_of_month.weekday() - chore_day) % 7
+            last_chore_day = last_of_month - timedelta(days=last_chore_day_offset)
+            last_chore_week = last_chore_day.isocalendar()[1]
+        else:
+            last_chore_week = last_of_month.isocalendar()[1]
         return last_chore_week - first_week + 1
 
     @staticmethod
@@ -741,7 +744,7 @@ class MonthlyChore(Chore):
             week_number
             if week_number > 0
             else max(
-                MonthlyChore.viable_weeks_in_month(date_of_month, chore_day) + week_number + 1,
+                MonthlyChore.viable_weeks_in_month(date_of_month, chore_day, False) + week_number + 1,
                 1
             )
         )
@@ -760,14 +763,14 @@ class MonthlyChore(Chore):
             weekday_number
             if weekday_number > 0
             else max(
-                MonthlyChore.viable_weeks_in_month(date_of_month, chore_day) + weekday_number + 1,
+                MonthlyChore.viable_weeks_in_month(date_of_month, chore_day, True) + weekday_number + 1,
                 1
             )
         )
 
         # 1st of the month is before the day of chore
         # (so 1st chore week the week when month starts)
-        if chore_day >= first_of_month.weekday():
+        if chore_day >= first_of_month.weekday() or weekday_number < 0:
             return first_of_month + relativedelta(
                 days=chore_day - first_of_month.weekday() + (actual_weekday_number - 1) * 7
             )
@@ -775,8 +778,9 @@ class MonthlyChore(Chore):
             days=7 - first_of_month.weekday() + chore_day + (actual_weekday_number - 1) * 7
         )
 
-    def _monthly_candidate(self, day1: date, start_date: date) -> date:
-        """Calculate possible date, for monthly frequency."""
+    def _monthly_candidate(self, day1: date, start_date: date) -> Tuple[date, int]:
+        """Calculate possible date, for monthly frequency.
+            2nd value is the month to consider the date in, even if different."""
         if self._chore_day is None:
             day_of_month = self._day_of_month
             if self._day_of_month is None:
@@ -788,10 +792,10 @@ class MonthlyChore(Chore):
                 )
 
             if day1.day <= day_of_month:
-                return date(day1.year, day1.month, day_of_month)
+                return (date(day1.year, day1.month, day_of_month), day1.month)
             if day1.month == 12:
-                return date(day1.year + 1, 1, day_of_month)
-            return date(day1.year, day1.month + 1, day_of_month)
+                return (date(day1.year + 1, 1, day_of_month), 1)
+            return (date(day1.year, day1.month + 1, day_of_month), day1.month + 1)
         if self._monthly_force_week_numbers:
             if self._week_order_number is not None:
                 candidate_date = MonthlyChore.nth_week_date(
@@ -799,7 +803,7 @@ class MonthlyChore(Chore):
                 )
                 # date is today or in the future -> we have the date
                 if candidate_date >= day1:
-                    return candidate_date
+                    return (candidate_date, day1.month)
         else:
             if self._weekday_order_number is not None:
                 candidate_date = MonthlyChore.nth_weekday_date(
@@ -809,22 +813,22 @@ class MonthlyChore(Chore):
                 )
                 # date is today or in the future -> we have the date
                 if candidate_date >= day1:
-                    return candidate_date
+                    return (candidate_date, day1.month)
         if day1.month == 12:
             next_chore_month = date(day1.year + 1, 1, 1)
         else:
             next_chore_month = date(day1.year, day1.month + 1, 1)
         if self._monthly_force_week_numbers:
-            return MonthlyChore.nth_week_date(
+            return (MonthlyChore.nth_week_date(
                 self._week_order_number,
                 next_chore_month,
                 WEEKDAYS.index(self._chore_day),
-            )
-        return MonthlyChore.nth_weekday_date(
+            ), next_chore_month.month)
+        return (MonthlyChore.nth_weekday_date(
             self._weekday_order_number,
             next_chore_month,
             WEEKDAYS.index(self._chore_day),
-        )
+        ), next_chore_month.month)
 
     def _find_candidate_date(self, day1: date) -> date | None:
         schedule_start_date = self._calculate_schedule_start_date()
@@ -835,12 +839,17 @@ class MonthlyChore(Chore):
             else:
                 day1 = date(day1.year, day1.month + 1, 1)
         if self._period is None or self._period == 1:
-            return self._monthly_candidate(day1, schedule_start_date)
-        candidate_date = self._monthly_candidate(day1, schedule_start_date)
-        while (candidate_date.month - schedule_start_date.month) % self._period != 0:
-            candidate_date = self._monthly_candidate(
-                candidate_date + relativedelta(days=1), schedule_start_date
+            return self._monthly_candidate(day1, schedule_start_date)[0]
+        result = self._monthly_candidate(day1, schedule_start_date)
+        candidate_date = result[0]
+        candidate_month = result[1]
+        while (candidate_month - schedule_start_date.month) % self._period != 0:
+            remainder = (candidate_date.month - schedule_start_date.month) % self._period
+            result = self._monthly_candidate(
+                candidate_date + relativedelta(months=remainder, day=1), schedule_start_date
             )
+            candidate_date = result[0]
+            candidate_month = result[1]
         return candidate_date
 
 
